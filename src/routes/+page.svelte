fa8a27c833b1b161be5cab4166a8688c578b4dc6
Ryi <satriaridho4355@gmail.com>
2026-08-20 13:19:30 +0700

    fix : change root redirect to client side

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
-<svelte:head>
-	<title>Redirecting... | Sakala Console</title>
-</svelte:head>
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { resolveRoute } from '$app/paths';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		goto(resolveRoute('/login'), { replaceState: true });
+	});
+</script>
